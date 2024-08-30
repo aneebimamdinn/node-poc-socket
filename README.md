ddad0266512f3634656d3370db04fae119d2ac83
Aneeb Imamdin <aneebimamdin@gmail.com>
2024-08-31 02:41:48 +0500

    Added readme and fix pushing again issue

---
+++ README.md
@@ -0,0 +1,22 @@
+# Node.js POC
+
+## Overview
+
+This is a simple Node.js application that uses Socket.IO for real-time events sync b/w users.
+
+## Prerequisites
+
+Make sure you have the following installed:
+
+- [Node.js](https://nodejs.org/) (version 14.x or later)
+- [npm](https://www.npmjs.com/) (comes with Node.js)
+
+## Getting Started
+
+Follow these steps to get the application up and running on your local machine.
+
+```bash
+git clone <repository-url>
+cd <repository-folder>
+npm i
+npm run start
--- index.js
+++ index.js
@@ -5,18 +5,27 @@ const io = require('socket.io')(server);
 const PORT = 4000;
 
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`App running on PORT : ${PORT}`);
 });
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/grid.html');
 });
 
 const events = [];
 io.on('connection', (socket)=>{
     socket.on("userDraw", (data)=>{
-        events.push(data.msg);
+        //Check if the element is already present it means its undo
+        //So remove the element instead of adding
+        const index = events.indexOf(data.msg);
+        // Check if the element exists in the array
+        if (index !== -1) {
+            // Remove the element using splice
+            events.splice(index, 1);
+        }else{
+            events.push(data.msg);
+        }
         io.emit('userDraw', data);
     });
 
